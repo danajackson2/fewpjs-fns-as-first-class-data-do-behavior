a67dd3aea7e701cba293a7eb2ec7cfadb3b5ba9d
danajackson2 <danajackson2@gmail.com>
2021-01-11 16:57:42 -0800

    fun fun

--- index.js
+++ index.js
@@ -9,5 +9,19 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
-/* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+function greet(time){
+  if (parseInt(time.split(':')[0]) < 12) {
+    greeting =  "Good Morning"
+  }
+  else if (parseInt(time.split(':')[0]) < 17) {
+    greeting = "Good Afternoon"
+  }
+  else {
+    greeting = "Good Evening"
+  }
+  return greeting
+}
+
+function displayMessage(string) {
+  document.getElementById('greeting').innerHTML = string
+}
